e7aa559224f68c3d7452a51e2f3d8d5cdb4e1471
Richard <x7536110@outlook.com>
2017-02-17 21:02:10 +0800

    bug fix

--- MD5.cpp
+++ MD5.cpp
@@ -287,7 +287,8 @@ int _httoi(const char *value)
     int result = 0;
     if (*s == '0' && *(s + 1) == 'X') s += 2;
     bool firsttime = true;
-    while (*s != '/0')
+    while (*s != '\0')
+    //while(1)
     {
         bool found = false;
         for (int i = 0; i < HexMapL; i++)
